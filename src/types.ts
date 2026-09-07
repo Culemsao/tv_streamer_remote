27ef154669209965798be41e66695c1d8d8e1959
Jasper <54394352+Culemsao@users.noreply.github.com>
2026-09-07 08:04:18 +0200

    apps
    added app shortcuts to remote

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,20 @@
+export interface AppDef {
+  name: string;
+  scheme: string;
+  match: string[];
+}
+
 export interface GoogleTVRemoteConfig {
   remote_entity: string;
   media_entity: string;
+  volume_entity?: string;
   title?: string;
+  show_title?: boolean;
+  show_navigation?: boolean;
+  show_buttons?: boolean;
+  show_apps?: boolean;
+  show_volume?: boolean;
+  label_navigation?: string;
+  label_volume?: string;
+  apps?: string[];
 }
